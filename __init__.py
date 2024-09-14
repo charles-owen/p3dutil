2b1f85d4783e0377c3c3930386c040de3ec4555a
Dr. Charles B. Owen <cbowen@msu.edu>
2024-09-13 20:33:36 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .NodePathUser import NodePathUser
-from .ActorUser import ActorUser
-from .Crosshair import Crosshair
-from .Axis import Axis
-from .Lines import Lines
-from .transforms import Transforms
-from .projector import Projector
